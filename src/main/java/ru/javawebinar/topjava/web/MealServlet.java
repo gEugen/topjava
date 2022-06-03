4daae68c24830f550f31b74fab9c06611dc443a9
gEugen <104776327+gEugen@users.noreply.github.com>
2022-06-03 19:14:55 +0400

    Implement a servlet according to the main learning task.
    - Add src/main/java/ru/javawebinar/topjava/util/HardCodeMealsListUtil
    .java for getting meal hardcode list;
    
    - Refactor main method for getting meal hardcode list by
    HardCodeMealsListUtil.java;
    
    - Add maven dependencies: javax.servlet.jsp.jstl and taglibs to pom.xml;
    
    - Modify src/main/java/ru/javawebinar/topjava/web/MealServlet.java to
    go to Meals web page with meal table;
    
    - Modify src/main/webapp/meals.jsp to get Meals web page with
    meal table;
    
    - Add getters to src/main/java/ru/javawebinar/topjava/model/MealTo.java
     to get field values of mealTo list items by src/main/webapp/meals.jsp.

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,12 +1,17 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.HardCodeMealsListUtil;
+import ru.javawebinar.topjava.util.MealsUtil;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -17,7 +22,13 @@ public class MealServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.debug("redirect to meals");
 
+        List<MealTo> mealToList = HardCodeMealsListUtil.getHardCodeMealsTo();
+
 //        req.getRequestDispatcher("/meals.jsp").forward(req, resp);
-        resp.sendRedirect("meals.jsp");
+//        resp.sendRedirect("meals.jsp");
+
+        RequestDispatcher view = req.getRequestDispatcher("meals.jsp");
+        req.setAttribute("mealsTo", mealToList);
+        view.forward(req, resp);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,3 +1,5 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 <%@ page contentType="text/html;charset=UTF-8" %>
 <html lang="ru">
 <head>
@@ -7,5 +9,31 @@
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+
+<table border=1>
+    <thead>
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    <jsp:useBean id="mealsTo" scope="request" type="java.util.List"/>
+    <c:forEach items="${mealsTo}" var="MealTo">
+        <tr>
+            <tr style="color:${MealTo.excess ? 'red' : 'green'}">
+            <td>
+                <fmt:parseDate  value="${MealTo.dateTime}" type="date" pattern="yyyy-MM-dd'T'HH:mm" var="parsedDate"/>
+                <fmt:formatDate value="${parsedDate}" type="date" pattern="yyyy-MM-dd HH:mm"/>
+            </td>
+            <td><c:out value="${MealTo.description}" /></td>
+            <td><c:out value="${MealTo.calories}"/></td>
+        </tr>
+    </c:forEach>
+
+    </tbody>
+</table>
 </body>
 </html>
