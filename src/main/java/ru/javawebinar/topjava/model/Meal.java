cb06a7e6856e9f257ede999003b7031ff49fd196
gEugen <104776327+gEugen@users.noreply.github.com>
2022-06-18 13:22:56 +0400

    Rework InMemoryMealRepository.java
    - provide the user to review and edit only their own food
    
    - add a unique user ID to the Meal.java
    
    - add a unique user ID to MealRepository.java interface methods
    
    - add an authorized user ID to arguments of MealServlet methods
    
    - add the user with non-authorized ID to user initial list
    
    - provide meal list output in reverse order

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -13,24 +13,19 @@ public class Meal extends AbstractBaseEntity {
 
     private final int calories;
 
-    public Meal(LocalDateTime dateTime, String description, int calories) {
-        this(null, dateTime, description, calories);
+    private Integer userId;
+
+    public Meal(LocalDateTime dateTime, String description, int calories, Integer userId) {
+        this(null, dateTime, description, calories, userId);
     }
 
-    public Meal(Integer id, LocalDateTime dateTime, String description, int calories) {
+    public Meal(Integer id, LocalDateTime dateTime, String description, int calories, Integer userId) {
         super(id);
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+        this.userId = userId;
     }
-
-//    public Integer getId() {
-//        return id;
-//    }
-
-//    public void setId(Integer id) {
-//        this.id = id;
-//    }
 
     public LocalDateTime getDateTime() {
         return dateTime;
@@ -54,6 +49,14 @@ public class Meal extends AbstractBaseEntity {
 
     public boolean isNew() {
         return id == null;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
     }
 
     @Override
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -5,15 +5,20 @@ import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
-    private final AtomicInteger counter = new AtomicInteger(0);
+    private final Map<Integer, Meal> repository;
+    private final AtomicInteger counter = new AtomicInteger();
 
-    {
+    public InMemoryMealRepository() {
+        this.repository = new ConcurrentHashMap<>();
         MealsUtil.meals.forEach(this::save);
     }
 
@@ -24,23 +29,45 @@ public class InMemoryMealRepository implements MealRepository {
             repository.put(meal.getId(), meal);
             return meal;
         }
+
         // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        return meal.getUserId() == repository.computeIfPresent(
+                        meal.getId(), (id, oldMeal) ->
+                                meal.getUserId() == repository.get(meal.getId()).getUserId() ? meal : oldMeal)
+                .getUserId() ? meal : null;
     }
 
     @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    public boolean delete(int id, int userId) {
+        AtomicBoolean result = new AtomicBoolean(false);
+        repository.computeIfPresent(id, (v, meal) -> {
+            if (repository.get(id).getUserId().equals(userId)) {
+                result.set(true);
+                return repository.remove(id);
+            }
+            return meal;
+        });
+        return result.get();
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int id, int userId) {
+        AtomicReference<Meal> result = new AtomicReference<>();
+        repository.computeIfPresent(id, (key, meal) -> {
+            if (meal.getUserId().equals(userId)) {
+                result.set(meal);
+            }
+            return meal;
+        });
+        return result.get();
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public Collection<Meal> getAll(Integer authUserId) {
+        return repository.values().stream()
+                .filter(meal -> meal.getUserId().equals(authUserId))
+                .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+                .collect(Collectors.toList());
     }
 }
 
