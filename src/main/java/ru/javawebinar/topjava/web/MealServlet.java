796d753fd81512237c2d689412bc6b5988f649b3
gEugen <104776327+gEugen@users.noreply.github.com>
2022-06-19 19:31:33 +0400

    Initialize Spring context in MealServlet and work via MealRestController

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -2,10 +2,13 @@ package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.repository.inmemory.InMemoryMealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.meal.MealRestController;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,19 +18,39 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 import java.util.Objects;
 
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
+import static ru.javawebinar.topjava.util.MealsUtil.getTos;
 import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
 
     private MealRepository repository;
+    private ConfigurableApplicationContext appCtx;
+    private MealRestController controller;
+    private final boolean springContextEn = true;
 
     @Override
     public void init() {
-        repository = new InMemoryMealRepository();
+        if (springContextEn) {
+            log.debug("Spring context enable");
+            appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+            controller = appCtx.getBean(MealRestController.class);
+            log.debug("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+        } else {
+            log.debug("MealServlet repository direct connection enable");
+            repository = new InMemoryMealRepository();
+        }
+    }
+
+    @Override
+    public void destroy() {
+        log.debug("Spring context close");
+        appCtx.close();
+        super.destroy();
     }
 
     @Override
@@ -41,8 +64,18 @@ public class MealServlet extends HttpServlet {
                 Integer.parseInt(request.getParameter("calories")));
 
         log.info(meal.isNew() ? "Create {}" : "Update {}", meal);
-        meal.setUserId(authUserId());
-        repository.save(meal);
+
+        if (springContextEn) {
+            if (meal.isNew()) {
+                controller.create(meal);
+            } else {
+                controller.update(meal, meal.getId());
+            }
+        } else {
+            meal.setUserId(authUserId());
+            repository.save(meal);
+        }
+
         response.sendRedirect("meals");
     }
 
@@ -54,23 +87,28 @@ public class MealServlet extends HttpServlet {
             case "delete":
                 int id = getId(request);
                 log.info("Delete id={}", id);
-                repository.delete(id, authUserId());
+
+                if (springContextEn) {
+                    controller.delete(id);
+                } else {
+                    repository.delete(id, authUserId());
+                }
+
                 response.sendRedirect("meals");
                 break;
             case "create":
             case "update":
                 final Meal meal = "create".equals(action) ?
-                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
-                        repository.get(getId(request), authUserId());
+                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) : springContextEn ?
+                                controller.get(getId(request)) : repository.get(getId(request), authUserId());
                 request.setAttribute("meal", meal);
                 request.getRequestDispatcher("/mealForm.jsp").forward(request, response);
                 break;
             case "all":
             default:
                 log.info("getAll");
-                request.setAttribute("meals",
-                        MealsUtil.getTos(repository.getAll(authUserId(), LocalDate.MIN, LocalDate.MAX),
-                                DEFAULT_CALORIES_PER_DAY));
+                request.setAttribute("meals", springContextEn ? controller.getAll() :
+                        getTos(repository.getAll(authUserId(), LocalDate.MIN, LocalDate.MAX), DEFAULT_CALORIES_PER_DAY));
                 request.getRequestDispatcher("/meals.jsp").forward(request, response);
                 break;
         }
