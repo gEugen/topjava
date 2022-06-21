54e8632882187efe2a8d9473e2582819680aad06
gEugen <104776327+gEugen@users.noreply.github.com>
2022-06-21 20:30:09 +0400

    Remove direct MealServlet connection to repository
    - remove Spring context control from UserServlet additionally

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -5,8 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.repository.MealRepository;
-import ru.javawebinar.topjava.repository.inmemory.InMemoryMealRepository;
 import ru.javawebinar.topjava.web.meal.MealRestController;
 
 import javax.servlet.ServletException;
@@ -16,37 +14,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.Arrays;
 import java.util.Objects;
-
-import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
-import static ru.javawebinar.topjava.util.MealsUtil.getTos;
-import static ru.javawebinar.topjava.web.SecurityUtil.authUserId;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(MealServlet.class);
-
-    private MealRepository repository;
     private ConfigurableApplicationContext appCtx;
     private MealRestController controller;
-    private final boolean springContextEn = true;
 
     @Override
     public void init() {
-        if (springContextEn) {
-            log.debug("Spring context enable");
-            appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
-            controller = appCtx.getBean(MealRestController.class);
-            log.debug("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-        } else {
-            log.debug("MealServlet repository direct connection enable");
-            repository = new InMemoryMealRepository();
-        }
+        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
+        controller = appCtx.getBean(MealRestController.class);
     }
 
     @Override
     public void destroy() {
-        log.debug("Spring context close");
         appCtx.close();
         super.destroy();
     }
@@ -62,15 +44,10 @@ public class MealServlet extends HttpServlet {
                 Integer.parseInt(request.getParameter("calories")));
 
         log.info(meal.isNew() ? "Create {}" : "Update {}", meal);
-
-        if (springContextEn) {
-            if (meal.isNew()) {
-                controller.create(meal);
-            } else {
-                controller.update(meal, meal.getId());
-            }
+        if (meal.isNew()) {
+            controller.create(meal);
         } else {
-            repository.save(meal, authUserId());
+            controller.update(meal, meal.getId());
         }
 
         response.sendRedirect("meals");
@@ -84,28 +61,20 @@ public class MealServlet extends HttpServlet {
             case "delete":
                 int id = getId(request);
                 log.info("Delete id={}", id);
-
-                if (springContextEn) {
-                    controller.delete(id);
-                } else {
-                    repository.delete(id, authUserId());
-                }
-
+                controller.delete(id);
                 response.sendRedirect("meals");
                 break;
             case "create":
             case "update":
                 final Meal newMeal = "create".equals(action) ?
-                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) : springContextEn ?
-                        controller.get(getId(request)) : repository.get(getId(request), authUserId());
+                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) : controller.get(getId(request));
                 request.setAttribute("meal", newMeal);
                 request.getRequestDispatcher("/mealForm.jsp").forward(request, response);
                 break;
             case "all":
             default:
                 log.info("getAll");
-                request.setAttribute("meals", springContextEn ? controller.getAll() :
-                        getTos(repository.getSomeViaPredicateFilter(authUserId(), meal -> true), DEFAULT_CALORIES_PER_DAY));
+                request.setAttribute("meals", controller.getAll());
                 request.getRequestDispatcher("/meals.jsp").forward(request, response);
                 break;
         }
--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,47 +1,17 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.javawebinar.topjava.repository.UserRepository;
-import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
-import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class UserServlet extends HttpServlet {
     private static final Logger log = getLogger(UserServlet.class);
-
-    private UserRepository repository;
-    private ClassPathXmlApplicationContext appCtx;
-    private AdminRestController controller;
-    private final boolean springContextEn = true;
-
-    @Override
-    public void init() {
-        if (springContextEn) {
-            log.debug("Spring context enable");
-            appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
-            controller = appCtx.getBean(AdminRestController.class);
-            log.debug("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-        } else {
-            log.debug("UserServlet repository direct connection enable");
-            repository = new InMemoryUserRepository();
-        }
-    }
-
-    @Override
-    public void destroy() {
-        log.debug("Spring context close");
-        appCtx.close();
-        super.destroy();
-    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
