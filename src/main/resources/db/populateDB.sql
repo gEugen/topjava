7a5e9419821e250abe382782a03119575b504a0b
gEugen <104776327+gEugen@users.noreply.github.com>
2022-06-28 19:07:44 +0400

    Create and initialize the database with the MEALS table additionally

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -10,3 +11,12 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (description, date_time, calories, user_id)
+VALUES ('Завтрак', '2020-01-30 10:00', 500, 100000),
+       ('Обед', '2020-01-30 13:00', 1000, 100000),
+       ('Ужин', '2020-01-30 20:00', 500, 100000),
+       ('Еда на границе', '2020-01-31 00:00', 100, 100001),
+       ('Завтрак', '2020-01-31 10:00', 1000, 100001),
+       ('Обед', '2020-01-31 13:00', 500, 100001),
+       ('Ужин2', '2020-01-31 20:00', 410, 100001);
